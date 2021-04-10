ffb537893be8c02fbca7daa549a70ebecdd893f5
Kuinox <nvandeginste@gmail.com>
2021-04-10 03:39:27 +0200

    Commented more unimplemented stuff.

--- CK.MQTT.P2P/OutgoingConnectAck.cs
+++ CK.MQTT.P2P/OutgoingConnectAck.cs
@@ -1,27 +1,27 @@
-using System;
+//using System;
 
-namespace CK.MQTT
-{
-    public class OutgoingConnectAck : SimpleOutgoingPacket
-    {
-        readonly ConnectReturnCode _connectReturnCode;
-        readonly SessionState _sessionState;
-        public OutgoingConnectAck( SessionState existingSession, ConnectReturnCode status )
-        {
-            _connectReturnCode = status;
-            _sessionState = existingSession;
-        }
+//namespace CK.MQTT
+//{
+//    public class OutgoingConnectAck : SimpleOutgoingPacket
+//    {
+//        readonly ConnectReturnCode _connectReturnCode;
+//        readonly SessionState _sessionState;
+//        public OutgoingConnectAck( SessionState existingSession, ConnectReturnCode status )
+//        {
+//            _connectReturnCode = status;
+//            _sessionState = existingSession;
+//        }
 
-        /// <inheritdoc/>
-        public override int GetSize( ProtocolLevel protocolLevel ) => 4;
+//        /// <inheritdoc/>
+//        public override int GetSize( ProtocolLevel protocolLevel ) => 4;
 
-        /// <inheritdoc/>
-        protected override void Write( ProtocolLevel protocolLevel, Span<byte> span )
-        {
-            span[0] = (byte)PacketType.ConnectAck;
-            span[1] = 2;
-            span[2] = (byte)_sessionState;
-            span[3] = (byte)_connectReturnCode;
-        }
-    }
-}
+//        /// <inheritdoc/>
+//        protected override void Write( ProtocolLevel protocolLevel, Span<byte> span )
+//        {
+//            span[0] = (byte)PacketType.ConnectAck;
+//            span[1] = 2;
+//            span[2] = (byte)_sessionState;
+//            span[3] = (byte)_connectReturnCode;
+//        }
+//    }
+//}
