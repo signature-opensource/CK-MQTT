87fbaa893fce18c4167aa4015bac2b1f73ebee92
Kuinox <nvandeginste@gmail.com>
2021-04-10 03:36:13 +0200

    Commented out WIP implementation.

--- CK.MQTT.Server/MqttServer.cs
+++ CK.MQTT.Server/MqttServer.cs
@@ -1,33 +1,33 @@
-using CK.Core;
-using System;
-using System.Threading.Tasks;
+//using CK.Core;
+//using System;
+//using System.Threading.Tasks;
 
-namespace CK.MQTT.Server
-{
-    class MqttServer
-    {
-        IMqttChannelListener _mqttChannelListener;
-        Task AcceptLoop()
-        {
-            var m = new ActivityMonitor();
-            while( true )
-            {
-                try
-                {
-                    var channel = await _mqttChannelListener.AcceptIncomingConnection()
-                }
-                catch( Exception e )
-                {
-                    m.Error( e );
-                }
-            }
-        }
+//namespace CK.MQTT.Server
+//{
+//    class MqttServer
+//    {
+//        IMqttChannelListener _mqttChannelListener;
+//        Task AcceptLoop()
+//        {
+//            var m = new ActivityMonitor();
+//            while( true )
+//            {
+//                try
+//                {
+//                    //var channel = await _mqttChannelListener.AcceptIncomingConnection();
+//                }
+//                catch( Exception e )
+//                {
+//                    m.Error( e );
+//                }
+//            }
+//        }
 
-        async ValueTask AcceptClient( IMqttChannel channel )
-        {
-            var instance = new ClientInstance( channel );
+//        async ValueTask AcceptClient( IMqttChannel channel )
+//        {
+//            var instance = new ClientInstance( channel );
             
-        }
+//        }
 
-    }
-}
+//    }
+//}
