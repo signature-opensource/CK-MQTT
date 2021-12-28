8c8f7c0603163cfcbb379f72bbe70c1d9ca045d7
Kuinox <nicolas.vandeginste@signature-code.com>
2021-12-28 22:05:46 +0100

    Removed CK.MQTT.Server.

--- CK.MQTT.Server/ClientInstance.cs
+++ CK.MQTT.Server/ClientInstance.cs
@@ -1,28 +1,28 @@
-using CK.Core;
-using System.Threading.Tasks;
+//using CK.Core;
+//using System.Threading.Tasks;
 
-namespace CK.MQTT.Server
-{
-    class ClientInstance
-    {
-        readonly MqttServer _mqttServer;
-        readonly IMqttChannel _channel;
+//namespace CK.MQTT.Server
+//{
+//    class ClientInstance
+//    {
+//        readonly MqttServer _mqttServer;
+//        readonly IMqttChannel _channel;
 
-        ClientInstance( MqttServer mqttServer, IMqttChannel channel )
-        {
-            _mqttServer = mqttServer;
-            _channel = channel;
-        }
-        public static ClientInstance HandleIncomingConnection( IActivityMonitor m, MqttServer server, IMqttChannel channel )
-        {
-            ClientInstance instance = new( server, channel );
+//        ClientInstance( MqttServer mqttServer, IMqttChannel channel )
+//        {
+//            _mqttServer = mqttServer;
+//            _channel = channel;
+//        }
+//        public static ClientInstance HandleIncomingConnection( IActivityMonitor m, MqttServer server, IMqttChannel channel )
+//        {
+//            ClientInstance instance = new( server, channel );
 
-            return instance;
-        }
+//            return instance;
+//        }
 
-        async Task HandleConnection()
-        {
-            _channel.DuplexPipe.Input.ReadAsync
-        }
-    }
-}
+//        async Task HandleConnection()
+//        {
+//            //_channel.DuplexPipe.Input.ReadAsync
+//        }
+//    }
+//}
--- CK.MQTT.Server/MqttServer.cs
+++ CK.MQTT.Server/MqttServer.cs
@@ -1,40 +1,40 @@
-using CK.Core;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
+//using CK.Core;
+//using System;
+//using System.Threading;
+//using System.Threading.Tasks;
 
-namespace CK.MQTT.Server
-{
-    class MqttServer
-    {
-        readonly IMqttChannelListener _mqttChannelListener;
-        async Task AcceptLoop()
-        {
-            var m = new ActivityMonitor();
-            while( true )
-            {
-                try
-                {
-                    (IMqttChannel channel, string clientAddress) = await _mqttChannelListener.AcceptIncomingConnection( _stopTokenCTS.Token );
-                    if( _stopTokenCTS.IsCancellationRequested )
-                    {
-                        m.Info( "Stop token triggered. Exiting accept loop." );
-                    }
-                    await AcceptClient( channel );
-                }
-                catch( Exception e )
-                {
-                    m.Error( e );
-                }
-            }
-        }
+//namespace CK.MQTT.Server
+//{
+//    class MqttServer
+//    {
+//        //readonly IMqttChannelListener _mqttChannelListener;
+//        async Task AcceptLoop()
+//        {
+//            var m = new ActivityMonitor();
+//            while( true )
+//            {
+//                try
+//                {
+//                    (IMqttChannel channel, string clientAddress) = await _mqttChannelListener.AcceptIncomingConnection( _stopTokenCTS.Token );
+//                    if( _stopTokenCTS.IsCancellationRequested )
+//                    {
+//                        m.Info( "Stop token triggered. Exiting accept loop." );
+//                    }
+//                    await AcceptClient( channel );
+//                }
+//                catch( Exception e )
+//                {
+//                    m.Error( e );
+//                }
+//            }
+//        }
 
-        CancellationTokenSource _stopTokenCTS;
+//        CancellationTokenSource _stopTokenCTS;
 
-        async ValueTask AcceptClient( IMqttChannel channel )
-        {
-            ClientInstance instance = new(this, channel );
-        }
+//        async ValueTask AcceptClient( IMqttChannel channel )
+//        {
+//            ClientInstance instance = new(this, channel );
+//        }
 
-    }
-}
+//    }
+//}
