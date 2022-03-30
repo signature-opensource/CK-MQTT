66bcce4061bb78ccdab17784e251061ebe765c81
Kuinox <nicolas.vandeginste@signature-code.com>
2022-03-30 11:29:18 +0200

    Fixed builds issues.

--- SimpleClientTest/Program.cs
+++ SimpleClientTest/Program.cs
@@ -15,42 +15,26 @@ namespace SimpleClientTest
 {
     class Program
     {
-        bool _isLowLevel;
-        async ValueTask MessageHandlerDelegate( string topic, PipeReader pipe, uint payloadLength, QualityOfService qos, bool retain, CancellationToken cancelToken )
-        {
-            if( !topic.Contains( "lowlevel" ) )
-            {
-                await new DisposableMessageClosure( OtherHandler ).HandleMessageAsync( topic, pipe, payloadLength, qos, retain, cancelToken );
-            }
-            else
-            {
-                // low level handling
-            }
-        }
 
-        static ValueTask OtherHandler( IActivityMonitor? m, DisposableApplicationMessage message, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
         static async Task Main()
         {
             //test.mosquitto.org
 
-            ActivityMonitor m = new();
-            var client = MqttClient.Factory.CreateMQTT3Client( new MqttClientConfiguration( "localhost:1883" )
-            {
-                KeepAliveSeconds = 3,
-                WaitTimeoutMilliseconds = 1000
-            },
-                MessageHandlerDelegate
-            );
-            //client.DisconnectedHandler += ( reason, task ) =>
+            //ActivityMonitor m = new();
+            //var client = new MqttClientImpl( new MqttClientConfiguration( "localhost:1883" )
             //{
-            //    System.Console.WriteLine( reason );
-            //};
-            var res = await client.ConnectAsync( m );
-            await Task.Delay( 3000000 );
-            await client.DisconnectAsync( m, true, true, default );
+            //    KeepAliveSeconds = 3,
+            //    WaitTimeoutMilliseconds = 1000
+            //},
+            //    MessageHandlerDelegate
+            //);
+            ////client.DisconnectedHandler += ( reason, task ) =>
+            ////{
+            ////    System.Console.WriteLine( reason );
+            ////};
+            //var res = await client.ConnectAsync( m );
+            //await Task.Delay( 3000000 );
+            //await client.DisconnectAsync( m, true, true, default );
         }
     }
 }
