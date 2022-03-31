719564e9aec40f133c7c1bb6853fbe3f5e4fb80e
Kuinox <github@kuinox.io>
2022-03-31 11:31:35 +0200

    Commented out test project.

--- SimpleP2PClientTest/Program.cs
+++ SimpleP2PClientTest/Program.cs
@@ -14,31 +14,31 @@ namespace SimpleP2PClientTest
 {
     class Program
     {
-        static ValueTask MessageHandlerDelegate( IActivityMonitor? m, ApplicationMessage msg, CancellationToken cancellationToken )
-        {
-            System.Console.WriteLine( msg.Topic + Encoding.UTF8.GetString( msg.Payload.Span ) );
-            return new ValueTask();
-        }
+        //static ValueTask MessageHandlerDelegate( IActivityMonitor? m, ApplicationMessage msg, CancellationToken cancellationToken )
+        //{
+        //    System.Console.WriteLine( msg.Topic + Encoding.UTF8.GetString( msg.Payload.Span ) );
+        //    return new ValueTask();
+        //}
         static async Task Main()
         {
             //test.mosquitto.org
-            var cfg = new GrandOutputConfiguration();
-            cfg.Handlers.Add( new ConsoleConfiguration() );
-            GrandOutput.EnsureActiveDefault( cfg );
+            //var cfg = new GrandOutputConfiguration();
+            //cfg.Handlers.Add( new ConsoleConfiguration() );
+            //GrandOutput.EnsureActiveDefault( cfg );
 
-            ActivityMonitor m = new();
-            P2PClient client = P2PMqttClient.Factory.CreateMQTTClient( new MqttClientConfiguration( "localhost:1883" )
-            {
-                KeepAliveSeconds = 0
-            }, MessageHandlerDelegate );
-            await client.AcceptClientAsync( m, new TcpChannelListener( new TcpListener( IPAddress.Any, 1883 ) ), default );
-            //while( true )
+            //ActivityMonitor m = new();
+            //P2PClient client = P2PMqttClient.Factory.CreateMQTTClient( new MqttClientConfiguration( "localhost:1883" )
             //{
-            //    string line = System.Console.ReadLine();
-            //    await client.PublishAsync( m, new ApplicationMessage( "foo", Encoding.UTF8.GetBytes( line ), QualityOfService.ExactlyOnce, false ) );
-            //}
-            await Task.Delay( -1 );
-            await client.DisconnectAsync( m, true, true, default );
+            //    KeepAliveSeconds = 0
+            //}, MessageHandlerDelegate );
+            //await client.AcceptClientAsync( m, new TcpChannelListener( new TcpListener( IPAddress.Any, 1883 ) ), default );
+            ////while( true )
+            ////{
+            ////    string line = System.Console.ReadLine();
+            ////    await client.PublishAsync( m, new ApplicationMessage( "foo", Encoding.UTF8.GetBytes( line ), QualityOfService.ExactlyOnce, false ) );
+            ////}
+            //await Task.Delay( -1 );
+            //await client.DisconnectAsync( m, true, true, default );
         }
     }
 }
